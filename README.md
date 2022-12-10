45665618f3643a895c5fbb6a4771ef6173832aa7
Sanket Naik <sanketnaik99@gmail.com>
2022-12-09 18:02:37 -0800

    Adds README, code formatting

---
+++ README.md
@@ -0,0 +1,112 @@
+# CSC 746 Project - Parallelization of Text Processing Algorithms
+
+## Requirements
+
+- Project Files
+- CMake
+- GNU Compiler
+- Dataset Files
+
+## Downloading the Project Files
+
+The project is available on Github and can be downloaded using the following command
+
+```bash
+git clone https://github.com/sanketnaik99/CSC746_Project.git
+```
+
+## How to download dataset files
+
+Please download the dataset files from this box link - [Box Drive Link](https://sfsu.box.com/s/cmq2vc6e1rlocxz81ha8dx2w8bd826ts)
+
+### Steps to set up the data
+
+- Please download all dataset files from the box drive link
+- create a new directory in the project folder by entering the following command
+
+  ```bash
+  mkdir data
+  ```
+
+- You can upload the dataset files in the data folder using the [NERSC File Browser](https://my.nersc.gov/filebrowser.php)
+- Kindly make sure that all dataset files are arranged as follows
+
+  ```bash
+  .
+  └── <project_root_folder>/
+      └── data/
+          ├── 10k-dataset.txt
+          ├── 100k-dataset.txt
+          ├── 1M-dataset.txt
+          └── 2M-dataset.txt
+  ```
+
+# Building and Running the Program
+
+All programs should run on a CORI KNL nodes. The directions for running the program are as follows -
+
+## Load Required Modules
+
+```bash
+module load cmake
+module swap PrgEnv-intel PrgEnv-gnu
+```
+
+## Request a CORI KNL Node in Interactive Mode
+
+```bash
+salloc --nodes 1 --qos interactive --time 01:00:00 --constraint knl --account m3930
+```
+
+## Build and Make the app
+
+```bash
+mkdir build
+cd build
+cmake ../
+make
+```
+
+## Run the Program
+
+```bash
+# make the job bash script an executable
+chmod +x ./job-openmp
+
+# Set the desired concurrency level
+export OMP_NUM_THREADS=1
+
+# Run the job - this will run the program on all 4 datasets
+./job-openmp
+```
+
+### Running using dynamic thread scheduling
+
+To run the program using dynamic thread scheduling, modify the job.in file as follows
+
+```bash
+# Comment out this line
+# export OMP_SCHEDULE=static
+
+# Uncomment this line
+export OMP_SCHEDULE=dynamic
+```
+
+Build and run the app again to verify the dynamic thread scheduling results.
+
+> **Note**: If the execution takes longer than 200 seconds, please stop the execution by pressing `CTRL+C`. This is because sometimes the program may run into an infinite loop due to an unknown bug.
+
+## Running the python program
+
+To run the python program, navigate out of the build directoy and run the script and provide the desired dataset as the second argument as follows
+
+```bash
+# Navigate out of the build directory
+cd ../
+
+# Run Python Script on different datasets
+python3 count_words.py data/10k-dataset.txt
+python3 count_words.py data/100k-dataset.txt
+python3 count_words.py data/1M-dataset.txt
+python3 count_words.py data/2M-dataset.txt
+```
--- omp_count_words.cpp
+++ omp_count_words.cpp
@@ -1,146 +1,139 @@
+#include "likwid-stuff.h"
+#include <boost/algorithm/string.hpp>
 #include <chrono>
-#include <iomanip>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
-#include <vector>
-#include <string.h>
-#include <sstream>
 #include <iterator>
 #include <map>
 #include <omp.h>
-#include "likwid-stuff.h"
-#include <boost/algorithm/string.hpp>
+#include <sstream>
+#include <string.h>
+#include <vector>
 
+void getStats(std::map<std::string, int> &M) {
 
-void getStats(std::map<std::string, int>& M)
-{
-  
-    // Declare a multimap
-    std::multimap<int, std::string> MM;
-  
-    // Insert every (key-value) pairs from
-    // map M to multimap MM as (value-key)
-    // pairs
-    for (auto& it : M) {
-        MM.insert({ it.second, it.first });
-    }
-  
-    // Print top 5 least frequently occuring words
-    auto it = MM.begin();
+  // Declare a multimap
+  std::multimap<int, std::string> MM;
 
-    std::cout << "The 5 least frequently occuring words are - " << std::endl;
-    for (int i = 0; i < 5; i++) {
-        std::cout << it->second << ' '
-             << it->first << std::endl;
-        it++;
-    }
+  // Insert every (key-value) pairs from
+  // map M to multimap MM as (value-key)
+  // pairs
+  for (auto &it : M) {
+    MM.insert({it.second, it.first});
+  }
 
-    // Print top 5 most frequently occuring words
-    auto it2 = MM.rbegin();
-    std::cout << "\n\nThe 5 most frequently occuring words are - " << std::endl;
-    for (int i = 0; i < 5; i++){
-        std::cout << it2->second << ' '
-             << it2->first << std::endl;
-        it2++;
-    }
-    std::cout << "\n" << std::endl;
+  // Print top 5 least frequently occuring words
+  auto it = MM.begin();
 
-    return;
+  std::cout << "The 5 least frequently occuring words are - " << std::endl;
+  for (int i = 0; i < 5; i++) {
+    std::cout << it->second << ' ' << it->first << std::endl;
+    it++;
+  }
+
+  // Print top 5 most frequently occuring words
+  auto it2 = MM.rbegin();
+  std::cout << "\n\nThe 5 most frequently occuring words are - " << std::endl;
+  for (int i = 0; i < 5; i++) {
+    std::cout << it2->second << ' ' << it2->first << std::endl;
+    it2++;
+  }
+  std::cout << "\n" << std::endl;
+
+  return;
 }
 
+int main(int argc, char **argv) {
 
-int main(int argc, char ** argv){
-    
-    // Get Input Filename from the commmand line
-    std::string inputFilename = argv[1];
+  // Get Input Filename from the commmand line
+  std::string inputFilename = argv[1];
 
-    std::cout << "The input filename is - " << inputFilename << std::endl;
-        
-    // Read file and store the data in vectors
-    // std::vector<std::vector<std::string>> allStrings;
-    std::vector<std::string> allStrings;
+  std::cout << "The input filename is - " << inputFilename << std::endl;
 
+  // Read file and store the data in vectors
+  // std::vector<std::vector<std::string>> allStrings;
+  std::vector<std::string> allStrings;
 
-    std::fstream dataFile;
-    dataFile.open(inputFilename, std::ios::in);
+  std::fstream dataFile;
+  dataFile.open(inputFilename, std::ios::in);
 
-    std::cout << "Reading File..." << std::endl;
+  std::cout << "Reading File..." << std::endl;
 
-    if (dataFile.is_open()){
-        std::string currentLine;
-        while(getline(dataFile, currentLine)){
-            // std::cout << "Current Line - " << currentLine << std::endl;
-            allStrings.push_back(currentLine);
+  if (dataFile.is_open()) {
+    std::string currentLine;
+    while (getline(dataFile, currentLine)) {
+      // std::cout << "Current Line - " << currentLine << std::endl;
+      allStrings.push_back(currentLine);
+    }
+  }
+
+  std::cout << "Done Reading File! Total Number of Lines = "
+            << allStrings.size() << std::endl;
+
+  std::cout << "MAX NUM THREADS - " << omp_get_max_threads() << std::endl;
+
+  // Go over every word in every line
+  int numStrings = allStrings.size();
+
+  std::map<std::string, int> wordMap;
+
+  std::map<int, int> threadWorkCount;
+
+  // Record Start Time
+  std::chrono::time_point<std::chrono::high_resolution_clock> start_time =
+      std::chrono::high_resolution_clock::now();
+
+#pragma omp parallel
+  {
+#pragma omp for
+    for (int i = 0; i < numStrings; i++) {
+
+      std::string word;
+      int thread_id = omp_get_thread_num();
+      std::vector<std::string> result;
+      boost::split(result, allStrings[i], boost::is_any_of(" "));
+
+      for (int i = 0; i < result.size(); i++) {
+        word = result[i];
+        std::map<std::string, int>::iterator it = wordMap.find(word);
+
+        if (it == wordMap.end()) {
+          wordMap.insert(std::pair<std::string, int>(word, 1));
+        } else {
+          it->second = it->second + 1;
         }
-    }
 
-    std::cout << "Done Reading File! Total Number of Lines = " << allStrings.size() << std::endl;
-
-    std::cout << "MAX NUM THREADS - " << omp_get_max_threads() << std::endl;
-
-    // Go over every word in every line
-    int numStrings = allStrings.size();
-
-    std::map<std::string, int> wordMap;
-    
-    std::map<int, int> threadWorkCount;
-
-    // Record Start Time
-    std::chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
-
-    #pragma omp parallel
-    {
-        #pragma omp for
-        for (int i = 0; i < numStrings; i++){
-            try {
-                std::string word;
-                int thread_id = omp_get_thread_num();
-                std::vector<std::string> result;
-                boost::split(result, allStrings[i], boost::is_any_of(" "));
-                for (int i = 0; i < result.size(); i++) {
-                    word = result[i];
-                    std::map<std::string, int>::iterator it = wordMap.find(word); 
-
-                    if (it == wordMap.end()){
-                        wordMap.insert(std::pair<std::string, int>(word, 1));
-                    } else{
-                        it->second = it->second + 1;
-                    }
-
-                    // Calculate the work done by each thread
-                    std::map<int, int>::iterator it2 = threadWorkCount.find(thread_id);
-                    if (it2 == threadWorkCount.end()){
-                        threadWorkCount.insert({thread_id, 1});
-                    } else{
-                        it2->second = it2->second + 1;
-                    }
-
-                }
-            } catch (...) {
-                std::cout << "Unknown exception at line - " << allStrings[i] << std::endl; 
-            }
-
+        // Calculate the work done by each thread
+        std::map<int, int>::iterator it2 = threadWorkCount.find(thread_id);
+        if (it2 == threadWorkCount.end()) {
+          threadWorkCount.insert({thread_id, 1});
+        } else {
+          it2->second = it2->second + 1;
         }
+      }
     }
+  }
 
+  // Record End Time
+  std::chrono::time_point<std::chrono::high_resolution_clock> end_time =
+      std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = end_time - start_time;
 
-    // Record End Time
-    std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = end_time - start_time;
+  std::cout << "\nWord Counting Completed!\n" << std::endl;
 
-    std::cout << "\nWord Counting Completed!\n" << std::endl;
+  getStats(wordMap);
 
-    getStats(wordMap);
+  std::cout << "\n Elapsed time is : " << elapsed.count()
+            << std::setprecision(8) << " " << std::endl;
 
-    std::cout << "\n Elapsed time is : " << elapsed.count() << std::setprecision(8) << " " << std::endl;
+  std::cout << "\n\nWork Done by Threads - " << std::endl;
 
-    std::cout << "\n\nWork Done by Threads - " << std::endl;
+  for (auto const &t : threadWorkCount) {
+    std::cout << "Thread ID - " << t.first << " | Total Number of words - "
+              << t.second << std::endl;
+  }
 
-    for (auto const &t: threadWorkCount) {
-        std::cout << "Thread ID - " << t.first << " | Total Number of words - "
-             << t.second << std::endl;
-    }
-
-    std::cout << "\n\n" << std::endl;
-    return 0;
+  std::cout << "\n\n" << std::endl;
+  return 0;
 }
